cd489019da9a75e3012688816fdc08698fdb9620
hitencodes01 <hitenunnao@gmail.com>
2025-07-26 15:29:10 +0530

    implementing put request in todos

--- app/page.js
+++ app/page.js
@@ -15,6 +15,6 @@ export default function Home() {
         <TodoInput />
         <TodoList />
       </div>
-    </main>
+    </main> 
   );
 }
--- app/todos/route.js
+++ app/todos/route.js
@@ -1,11 +1,16 @@
 import todoData from "../../todos";
+import {writeFile} from "fs/promises"
 export async function GET() {
   return Response.json(todoData);
-  // return new Response(JSON.stringify(todoData) , {
-  //     headers : {
-  //         "Content-Type" : "application/json",
-  //     },
-  // status : 210,
-  // statusText : "fuck"
-  // })
+}
+export async function POST(request){
+  const todo = await request.json()
+  const newTodo = {
+    id : crypto.randomUUID(),
+    text : todo.text,
+    completed : false
+  }
+  todoData.push(newTodo)
+  writeFile("todos.json", JSON.stringify(todoData , null , 2))
+  return Response.json(newTodo)
 }
