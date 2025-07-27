e33efa988c9e0166ac5a7b9c3f32120c012b05e4
hitencodes01 <hitenunnao@gmail.com>
2025-07-27 06:28:53 +0530

    implementing put property in slug

--- app/todos/[id]/route.js
+++ app/todos/[id]/route.js
@@ -1,7 +1,11 @@
+import { error } from "console";
 import todos from "../../../todos";
+import {writeFile} from 'fs/promises'
+
+// get code
 export async function GET(_, { params }) {
   const { id } = await params;
-  const todo = todos.find((todo) => id === String(todo.id));
+  const todo = todos.find((todo) => id === todo.id);
   if (!todo) {
     return Response.json(
       { error: "ToDo not found" },
@@ -11,4 +15,21 @@ export async function GET(_, { params }) {
     );
   }
   return Response.json(todo);
+}
+
+
+// update todo code
+export async function PUT(request , {params}){
+  const editedTodoData = await request.json()
+  console.log(editedTodoData)
+  const {id} = await params
+  const todoIndex = todos.findIndex((todo)=> id === todo.id)
+  const todo = todos[todoIndex]
+  if(editedTodoData.id){
+    return Response.json({error : "id change does not allowed here"})
+  }
+  const editedTodo = {...todo , ...editedTodoData}
+  todos[todoIndex] = editedTodo
+  writeFile('todos.json',JSON.stringify(todos,null,2))
+  return Response.json(editedTodo)
 }
