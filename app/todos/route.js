d08360eb85ebdc8f937da05caf1de98fd4404bb5
hitencodes01 <hitenunnao@gmail.com>
2025-07-28 22:58:21 +0530

    enabling get and  post route with mongoDB

--- app/todos/route.js
+++ app/todos/route.js
@@ -5,31 +5,27 @@ import { connectDB } from "@/lib/connectDB";
 
 export async function GET() {
   await connectDB()
-  // const result = await Todo.find()
-  // console.log(result)
-  const newTodo = await Todo.create({
-    text : "learn mongodb"
-  })
-  console.log(newTodo)
-  console.log("first")
-  console.log("first")
-  console.log("second")
-  const stringifyData = await readFile('./todos.json' , "utf-8")
-  const data = await JSON.parse(stringifyData)
-  return Response.json(newTodo);
+  const allTodo = await Todo.find()
+  console.log(allTodo)
+  return Response.json(allTodo.map(({id,text,completed}) => ({id,text,completed})))
+  // const stringifyData = await readFile('./todos.json' , "utf-8")
+  // const data = await JSON.parse(stringifyData)
+  // return Response.json(newTodo);
 }
 
 
 export async function POST(request){
   const todo = await request.json()
-  const newTodo = {
-    id : crypto.randomUUID(),
-    text : todo.text,
-    completed : false
-  }
-  todoData.push(newTodo)
-  writeFile("todos.json", JSON.stringify(todoData , null , 2))
-  return Response.json(newTodo , {
+  // const newTodo = {
+  //   id : crypto.randomUUID(),
+  //   text : todo.text,
+  //   completed : false
+  // }
+  const {id , text , completed} = await Todo.create({text : todo.text})
+
+  // todoData.push(newTodo)
+  // writeFile("todos.json", JSON.stringify(todoData , null , 2))
+  return Response.json({id , text , completed} , {
     status : 201
   })
 }
